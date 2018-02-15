a2307a4ed15c6ba0c65572f3507b07b01b3248d8
Unknown <seres.jozsef.dani@gmail.com>
2018-02-15 15:32:19 +0100

    Imporvements!
    Editable TextFields, Corrected ProgramWindow size, Added CheckBox Column

--- Tv/src/sample/Controller.java
+++ Tv/src/sample/Controller.java
@@ -2,12 +2,14 @@ package sample;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.Pane;
@@ -46,22 +48,60 @@ public class Controller implements Initializable{
     public void initialize(URL location, ResourceBundle resources) {
 
         TableColumn channelCol = new TableColumn("Csatorna");
-        channelCol.setMinWidth(10);
+        channelCol.setMinWidth(150);
+        channelCol.setStyle("-fx-alignment: CENTER;");
         channelCol.setCellFactory(TextFieldTableCell.forTableColumn());
         channelCol.setCellValueFactory(new PropertyValueFactory<Channel, String>("channel"));
 
+        channelCol.setOnEditCommit(
+                new EventHandler<TableColumn.CellEditEvent<Channel, String>>() {
+                    @Override
+                    public void handle(TableColumn.CellEditEvent<Channel, String> event) {
+                        ((Channel) event.getTableView().getItems().get(
+                                event.getTablePosition().getRow())).setChannel(event.getNewValue());
+                    }
+                }
+        );
+
 
         TableColumn frekCol = new TableColumn("Frekvencia");
-        frekCol.setMinWidth(15);
+        frekCol.setMinWidth(200);
+        frekCol.setStyle("-fx-alignment: CENTER;");
         frekCol.setCellFactory(TextFieldTableCell.forTableColumn());
         frekCol.setCellValueFactory(new PropertyValueFactory<Channel, String>("frek"));
 
+        frekCol.setOnEditCommit(
+                new EventHandler<TableColumn.CellEditEvent<Channel, String>>() {
+                    @Override
+                    public void handle(TableColumn.CellEditEvent<Channel, String> event) {
+                        ((Channel) event.getTableView().getItems().get(
+                                event.getTablePosition().getRow())).setFrek(event.getNewValue());
+                    }
+                }
+        );
+
         TableColumn nameCol = new TableColumn("Csatorna neve");
-        nameCol.setMinWidth(100);
+        nameCol.setMinWidth(250);
+        nameCol.setStyle("-fx-alignment: CENTER;");
         nameCol.setCellFactory(TextFieldTableCell.forTableColumn());
         nameCol.setCellValueFactory(new PropertyValueFactory<Channel, String>("name"));
 
-        table.getColumns().addAll(channelCol, frekCol, nameCol);
+        nameCol.setOnEditCommit(
+                new EventHandler<TableColumn.CellEditEvent<Channel, String>>() {
+                    @Override
+                    public void handle(TableColumn.CellEditEvent<Channel, String> event) {
+                        ((Channel) event.getTableView().getItems().get(
+                                event.getTablePosition().getRow())).setName(event.getNewValue());
+                    }
+                }
+        );
+
+        TableColumn checkCol = new TableColumn("Beállítva");
+        checkCol.setMinWidth(74);
+        checkCol.setStyle("-fx-alignment: Center;");
+        checkCol.setCellFactory(CheckBoxTableCell.forTableColumn(channelCol));
+
+        table.getColumns().addAll(channelCol, frekCol, nameCol, checkCol);
         table.setItems(data);
     }
 }
--- Tv/src/sample/Main.java
+++ Tv/src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -15,9 +16,10 @@ public class Main extends Application {
         Scene scene = new Scene(root);
 
         primaryStage.setTitle("Csatorna kiosztás");
-        primaryStage.setWidth(800);
-        primaryStage.setHeight(600);
+        primaryStage.setWidth(825 );
+        primaryStage.setHeight(650);
         primaryStage.setScene(scene);
+        primaryStage.getIcons().add(new Image("tv.png"));
         primaryStage.show();
     }
 
